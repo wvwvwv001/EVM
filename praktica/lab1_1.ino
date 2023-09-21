4a3a18f137f515c1d01f199907055e28803cbfdb
wvwvwv001 <99797060+wvwvwv001@users.noreply.github.com>
2023-09-21 12:32:13 +0500

    Update lab1_1.ino

--- praktica/lab1_1.ino
+++ praktica/lab1_1.ino
@@ -1,26 +1,13 @@
-const int buttonPin = D1;  // the number of the pushbutton pin
-const int ledPin = D0;    // the number of the LED pin
-
-// variables will change:
-int buttonState = 0;  // variable for reading the pushbutton status
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(D0, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
+  digitalWrite(D0, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(D0, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
 }
