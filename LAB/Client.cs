0e6029d97f928873aa4168d6888be4ef3125b28c
wvwvwv001 <99797060+wvwvwv001@users.noreply.github.com>
2023-09-22 14:35:36 +0500

    Add files via upload

---
+++ LAB/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+public struct Structure
+{
+    public int n;
+    public int m;
+}
+
+class Client
+{
+    static void Main()
+    {
+        // Открытие канала
+        using NamedPipeClientStream Client = new(".", "channel", PipeDirection.InOut);
+        Client.Connect();
+
+        // Получение данных от сервера
+        byte[] bytes = new byte[Unsafe.SizeOf<Structure>()];
+        Client.Read(bytes, 0, bytes.Length);
+        Structure receivedData = Unsafe.As<byte, Structure>(ref bytes[0]);
+        Console.WriteLine($"Received data: n = {receivedData.n}, m = {receivedData.m}");
+
+        // Изменение флага
+        receivedData.n *= receivedData.m;
+        
+        // Отправка измененных данных обратно на сервер
+        byte[] modified_bytes = new byte[Unsafe.SizeOf<Structure>()];
+        Unsafe.As<byte, Structure>(ref modified_bytes[0]) = receivedData;
+        Client.Write(modified_bytes, 0, modified_bytes.Length);
+    }
+}
---
+++ LAB/Server.cs
@@ -0,0 +1,36 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+public struct Structure
+{
+    public int n;
+    public int m;
+}
+
+class Server
+{
+    static void Main()
+    {
+        // Открытие канала
+        using NamedPipeServerStream Server = new("channel", PipeDirection.InOut);
+        Server.WaitForConnection();
+
+        // Создание сообщения
+        Structure msg = new()
+        {
+            n = 99,
+            m = 10,
+        };
+        
+        // Преобразование в байты
+        byte[] bytes = new byte[Unsafe.SizeOf<Structure>()];
+        Unsafe.As<byte, Structure>(ref bytes[0]) = msg;
+        Server.Write(bytes, 0, bytes.Length);
+
+        // Получение измененных данных от клиента
+        byte[] receivedBytes = new byte[Unsafe.SizeOf<Structure>()];
+        Server.Read(receivedBytes, 0, receivedBytes.Length);
+        Structure receivedData = Unsafe.As<byte, Structure>(ref receivedBytes[0]);
+        Console.WriteLine($"Received data: n = {receivedData.n}, m = {receivedData.m}");
+    }
+}
