398d53bcd22160e5543acce6f3bb5b16334654ff
wvwvwv001 <99797060+wvwvwv001@users.noreply.github.com>
2023-10-27 15:33:20 +0500

    Add files via upload

---
+++ LAB/Client_lab3.cs
@@ -0,0 +1,65 @@
+﻿using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+
+public struct Structure
+{
+    public double a;
+    public double b;
+    public double result;
+}
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            using (NamedPipeClientStream Client = new(".", args[0], PipeDirection.InOut))
+            {
+                Client.Connect();
+                try
+                {
+                    while (true)
+                    {
+                        byte[] bytes = new byte[Unsafe.SizeOf<Structure>()];
+                        Client.Read(bytes, 0, bytes.Length);
+                        Structure receivedData = Unsafe.As<byte, Structure>(ref bytes[0]);
+                        Console.WriteLine($"Received data: a = {receivedData.a}, b = {receivedData.b}");
+                        double a = receivedData.a;
+                        double b = receivedData.b;
+                        int n = 1000;
+
+                        receivedData.result = TrapezoidalRule(a, b, n);
+                        Console.WriteLine(receivedData.result);
+                        byte[] modified_bytes = new byte[Unsafe.SizeOf<Structure>()];
+                        Unsafe.As<byte, Structure>(ref modified_bytes[0]) = receivedData;
+                        Client.Write(modified_bytes, 0, modified_bytes.Length);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+    }
+    static double Function(double x)
+    {
+        return 2 * Math.Sin (x);
+    }
+
+    static double TrapezoidalRule(double a, double b, int n)
+    {
+        double h = (b - a) / Convert.ToDouble(n);
+        double result = 0.5 * (Function(a) + Function(b));
+
+        for (int i = 1; i < n; i++)
+        {
+            
+            double x = a + i * h;
+            result += Function(x);
+        }
+
+        result *= h;
+        Console.WriteLine(result);
+        return result;
+    }
+}
+
---
+++ LAB/Server_lab3.cs
@@ -0,0 +1,137 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+public struct Structure
+{
+    public double a;
+    public double b;
+    public double result;
+}
+
+class PipeServer
+{
+    private static PriorityQueue<Structure, int> dataQueue = new PriorityQueue<Structure, int>();
+    private static Mutex mutex = new Mutex();
+    private static Mutex mutFile = new Mutex();
+    private static int count = 0;
+    private static string path = "C:\\labs\\evm\\labC\\bin\\Debug\\net7.0\\labC.exe";
+
+    private static async Task Main()
+    {
+        CancellationTokenSource source = new CancellationTokenSource();
+        CancellationToken token = source.Token;
+       
+        StreamWriter file = new StreamWriter("output.txt", true);
+        string str = string.Empty;
+        Console.CancelKeyPress += (sender, eventArgs) =>
+        {
+            eventArgs.Cancel = true;
+            source.Cancel();
+        };
+
+        try
+        {
+            await Task.WhenAll(SenderTask(token), ReceiverTask(token));;
+        }
+        catch { }
+        finally {file.Close();}
+
+        Task SenderTask(CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    double _n, _m;
+                    int _priority;
+                    Console.Write("Enter a, b, priority: ");
+                    string? res = Console.ReadLine();
+                    if (res == null)
+                    {
+                        Console.WriteLine("Invalid input");
+                        
+                    }
+                    else
+                    {
+                        string[] parse = res.Split(' ');
+                        if (parse.Length == 2)
+                        {
+                            _n = Convert.ToDouble(parse[0]);
+                            _m  = Convert.ToDouble(parse[1]);
+                            _priority = 0;
+                        }
+                        else
+                        {
+                            _n = Convert.ToDouble(parse[0]);
+                            _m = Convert.ToDouble(parse[1]);
+                            _priority = int.Parse(parse[2]);
+                        }
+                        Structure data = new Structure
+                        {
+                            a = _n,
+                            b = _m,
+                        };
+                        mutex.WaitOne();
+                        dataQueue.Enqueue(data, _priority);
+                        mutex.ReleaseMutex();
+                    }
+                }
+            }); 
+        }
+
+        Task ReceiverTask(CancellationToken token)
+        {
+            return Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Structure st;
+                    int pr;
+                    mutex.WaitOne();
+                    bool flag = dataQueue.TryDequeue(out st, out pr);
+                    mutex.ReleaseMutex();
+                    if (flag)
+                    {
+                        ClientConnect(st, token);
+                    }
+                }
+            });
+        }
+
+        async void ClientConnect(Structure st, CancellationToken token)
+        {
+            try
+            {
+                byte[] dataBytes = new byte[Unsafe.SizeOf<Structure>()];
+                Unsafe.As<byte, Structure>(ref dataBytes[0]) = st;
+                NamedPipeServerStream pipeServer = new($"channel{count}", PipeDirection.InOut);
+                Console.WriteLine("Waiting for client connection...");
+                Process myProcess = new Process();
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = path;
+                myProcess.StartInfo.Arguments = $"channel{count}";
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.Start();
+                await pipeServer.WaitForConnectionAsync();
+                Console.WriteLine("Client connected");
+                await pipeServer.WriteAsync(dataBytes, 0, dataBytes.Length);
+                byte[] receivedBytes = new byte[Unsafe.SizeOf<Structure>()];
+                if (await pipeServer.ReadAsync(receivedBytes, 0, receivedBytes.Length) == receivedBytes.Length)
+                {
+                    st = Unsafe.As<byte, Structure>(ref receivedBytes[0]);
+                }
+             
+                mutFile.WaitOne();
+                file.WriteLine($"a = {st.a}; b = {st.b}; priority = {0}; result = {st.result}");
+                mutFile.ReleaseMutex();
+                pipeServer.Close();
+                count++;
+                await myProcess.WaitForExitAsync(token);
+            }
+            catch (Exception) { }
+        }
+    }
+
+}
